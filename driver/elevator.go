6f430d55e74bccad7d481120f6392a952f3b5396
patricknitschke <patrick.nitschke8@gmail.com>
2020-02-23 17:31:39 +0100

    start elevator module

--- driver/elevator.go
+++ driver/elevator.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
-
 )
+
+// Elevator maintains current state
+type Elevator struct {
+	dir          int
+	currentFloor int
+}
+
+var elevator Elevator
 
 func elevatorInit() {
 	fmt.Println("Initialised elevator module")
+	elevator.currentFloor = 0
+	elevator.dir = 0
 }
