ac034eb8f32ab667974ca44833eeed2020482738
patricknitschke <patrick.nitschke8@gmail.com>
2020-02-26 21:51:29 +0100

    add function expectations as comments

--- driver/watchdog.go
+++ driver/watchdog.go
@@ -8,9 +8,11 @@ import (
 const queueInterval = 15    // seconds
 const imAliveInterval = 100 // microseconds
 
+// Maybe variables could be structs instead of *Timers?
 var aliveTimer [NNodes]*time.Timer
 var queueTimer [NFloors][NButtonTypes]*time.Timer
 
+// Forever watches for aliveTimeouts -> calls watchAlert
 func watchPollAliveTimeout() {
 	for {
 		select {
@@ -22,18 +24,32 @@ func watchPollAliveTimeout() {
 	}
 }
 
+// Forever watches for queueTimeouts -> calls watchAlert
 func watchPollQueueTimeout() {
 
 }
 
+// Function that resets internalQ, asks for help and stuff
+func watchAlert() {
+
+}
+
+// Restarts the imAlive timer
 func watchSetAlive(elevatorID int) {
 	aliveTimer[elevatorID] = time.NewTimer(imAliveInterval * time.Millisecond)
 }
 
+// Starts a timer for a particular order
 func watchSetQueue(order Order) {
 	queueTimer[order.floor][int(order.orderT)] = time.NewTimer(queueInterval * time.Second)
 }
 
+// Disables a timer for a particular order
+func watchSPopQueue(order Order) {
+
+}
+
+// Timer for regular interval ImAlive broadcasts
 func watchBcastImAlive() {
 	for now := range time.Tick(imAliveInterval * time.Millisecond) {
 		//newtorkBcastImalive()
@@ -41,10 +57,7 @@ func watchBcastImAlive() {
 	}
 }
 
-func watchAlert() {
-
-}
-
+// Returns an bool array ([]bool) of alive nodes - could be used to update Decision module's static var?
 func watchCheckWhoAlive() {
 
 }
